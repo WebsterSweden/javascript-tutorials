b6fe9f0df49380834f8ecbf570ad771751bede83
WebsterSweden <webster@webster.se>
2020-09-20 04:27:09 +0200

    Edit number-guessing-game-1-main.js.

--- number-guessing-game/number-guessing-game-1-main.js
+++ number-guessing-game/number-guessing-game-1-main.js
@@ -7,31 +7,23 @@ window
 
 // console.log(document.currentScript);
 
-let userInput = document.getElementById("userInput");
-
-let userInputInterchange = userInput.value;
-// userInputInterchange = null;
-
-// let userInputRemoveAllWhitespace = userInput.value.replace(/\s+/g, "");
-let userInputRemoveAllWhitespace = userInputInterchange.replace(/\s+/g, "");
-// let userInputRemoveAllWhitespace;
-
-// userInput = undefined;
-// userInput = null;
-// userInput.value = "";
-// userInput.value = "A Whitespace B";
-
-console.log("userInput: " + userInput);
-console.log("userInput.value: " + userInput.value);
-console.log("userInputInterchange: " + userInputInterchange);
-console.log("userInputRemoveAllWhitespace: " + userInputRemoveAllWhitespace);
-
 function randomNumberFunction() {
     'use strict';
+    let userInput = document.getElementById("userInput");
+    let userInputInterchange = userInput.value;
+    let userInputRemoveAllWhitespace = userInputInterchange.replace(/\s+/g, "");
+    let userInputIsNotNumber = userInputRemoveAllWhitespace.replace(/[^0-9]/g);
+    let minimumNumber = 1;
+    let maximumNumber = 10;
+
+    // userInput = undefined;
+    // userInput = null;
+    // userInput.value = "";
+    // userInput.value = "A Whitespace B";
 
     if (userInput === undefined) { // Check if the input field is undefined.
         document.getElementById("output").innerText = "The input field is undefined";
-        console.log("unserInput is undefined");
+        console.log("userInput is undefined");
     }
 
     else if (userInput === null) { // Check if the input field is null.
@@ -39,29 +31,47 @@ function randomNumberFunction() {
         console.log("userInput is null");
     }
 
-    else if (userInput.value === "") { // Check if the input field is empty.
-        document.getElementById("output").innerText = "Field can not be blank";
+    else if (userInputInterchange === "") { // Check if the input field is empty.
+        document.getElementById("output").innerText = "Field can not be blank: " + "\" \"";
         console.log("input field is empty string");
     }
 
     else if (userInputInterchange !== userInputRemoveAllWhitespace) { // Check and remove all whitespaces.
-        // TODO: The whitespace check need two click. Change to one click.
-        userInputRemoveAllWhitespace = userInput.value.replace(/\s+/g, "");
         document.getElementById("output").innerText = "The input can not have whitespaces: " + userInputInterchange;
         console.log("input field have whitespaces");
-        // userInputInterchange = userInput.value;
+    }
+
+    else if (userInputIsNotNumber !== userInputRemoveAllWhitespace) { // Check only input of numbers.
+        document.getElementById("output").innerText = "The input can only have numbers: " + userInputRemoveAllWhitespace;
+        console.log("input field can only have numbers");
+    }
+
+    else if (userInputRemoveAllWhitespace < minimumNumber) { // Check if input number is greater than minimum number.
+        document.getElementById("output").innerText = "The input number have to be greater than: " + minimumNumber;
+        console.log("input field have to be greater");
+    }
+
+    else if (userInputRemoveAllWhitespace > maximumNumber) { // Check if input number is lesser than maximum number.
+        document.getElementById("output").innerText = "The input number have to be less than: " + maximumNumber;
+        console.log("input field have to be lesser");
     }
 
     else if (userInputInterchange === userInputRemoveAllWhitespace) { // Check and input value is valid.
-        userInputRemoveAllWhitespace = userInput.value.replace(/\s+/g, "");
         document.getElementById("output").innerText = "The input is valid: " + userInputRemoveAllWhitespace;
+        console.log("input field is valid");
+    }
+
+    else {
+        document.getElementById("output").innerText = "else";
+        console.log("else");
         console.log("userInput: " + userInput);
         console.log("userInput.value: " + userInput.value);
-        userInputInterchange = userInput.value;
         console.log("userInputInterchange: " + userInputInterchange);
         console.log("userInputRemoveAllWhitespace: " + userInputRemoveAllWhitespace);
     }
 
+    // document.getElementById("output").innerText = "The input value is: " + userInput.value;
+    // console.log("userInput.value: " + userInput.value);
 }
 
 document.getElementById("enterButton").addEventListener("click", randomNumberFunction);
