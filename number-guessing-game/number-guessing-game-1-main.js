3f8ac75a587755e7633cff3888ed0212e4f6f11f
WebsterSweden <webster@webster.se>
2020-09-20 19:56:22 +0200

    Edit number-guessing-game-1-main.js, number-guessing-game-1.html.

--- number-guessing-game/number-guessing-game-1-main.js
+++ number-guessing-game/number-guessing-game-1-main.js
@@ -7,14 +7,37 @@ window
 
 // console.log(document.currentScript);
 
+// Create a random number.
+let minimumNumber = 1;
+let maximumNumber = 10;
+
+// Create turn counter.
+let roundStart = 0;
+let roundCurrent = roundStart;
+let roundEnd;
+
+let randomNumberGenerated;
+
+// Minimum and maximum random numbers.
 function randomNumberFunction() {
+    'use strict';
+    randomNumberGenerated = Math.floor(Math.random(minimumNumber) * (maximumNumber) + 1);
+    // document.getElementById("minimumNumber").innerText = minimumNumber;
+    // document.getElementById("maximumNumber").innerText = maximumNumber;
+    document.getElementById("randomIntervalInfo").innerText = "Guess a number between " + minimumNumber + " - " + maximumNumber;
+    document.getElementById("randomNumberOutput").innerText = randomNumberGenerated;
+    console.log("randomNumberFunction: " + randomNumberGenerated);
+}
+
+document.getElementById("randomNumberCreate").addEventListener("click", randomNumberFunction);
+
+// Check user input and validate the data.
+function inputValidationFunction() {
     'use strict';
     let userInput = document.getElementById("userInput");
     let userInputInterchange = userInput.value;
     let userInputRemoveAllWhitespace = userInputInterchange.replace(/\s+/g, "");
     let userInputIsNotNumber = userInputRemoveAllWhitespace.replace(/[^0-9]/g);
-    let minimumNumber = 1;
-    let maximumNumber = 10;
 
     // userInput = undefined;
     // userInput = null;
@@ -24,44 +47,48 @@ function randomNumberFunction() {
     if (userInput === undefined) { // Check if the input field is undefined.
         document.getElementById("output").innerText = "The input field is undefined";
         console.log("userInput is undefined");
-    }
-
-    else if (userInput === null) { // Check if the input field is null.
+    } else if (userInput === null) { // Check if the input field is null.
         document.getElementById("output").innerText = "The input field is null";
         console.log("userInput is null");
-    }
-
-    else if (userInputInterchange === "") { // Check if the input field is empty.
+    } else if (userInputInterchange === "") { // Check if the input field is empty.
         document.getElementById("output").innerText = "Field can not be blank: " + "\" \"";
         console.log("input field is empty string");
-    }
-
-    else if (userInputInterchange !== userInputRemoveAllWhitespace) { // Check and remove all whitespaces.
+    } else if (userInputInterchange !== userInputRemoveAllWhitespace) { // Check and remove all whitespaces.
         document.getElementById("output").innerText = "The input can not have whitespaces: " + userInputInterchange;
         console.log("input field have whitespaces");
-    }
-
-    else if (userInputIsNotNumber !== userInputRemoveAllWhitespace) { // Check only input of numbers.
-        document.getElementById("output").innerText = "The input can only have numbers: " + userInputRemoveAllWhitespace;
-        console.log("input field can only have numbers");
-    }
-
-    else if (userInputRemoveAllWhitespace < minimumNumber) { // Check if input number is greater than minimum number.
-        document.getElementById("output").innerText = "The input number have to be greater than: " + minimumNumber;
+    } else if (userInputIsNotNumber !== userInputRemoveAllWhitespace) { // Check only input of integers.
+        document.getElementById("output").innerText = "The input can only have integers: " + userInputRemoveAllWhitespace;
+        console.log("input field can only have integers");
+    } else if (userInputRemoveAllWhitespace < minimumNumber) { // Check if input number is greater than minimum number.
+        document.getElementById("output").innerText = "The input number " + userInputInterchange + " have to be greater than: " + minimumNumber;
         console.log("input field have to be greater");
-    }
-
-    else if (userInputRemoveAllWhitespace > maximumNumber) { // Check if input number is lesser than maximum number.
-        document.getElementById("output").innerText = "The input number have to be less than: " + maximumNumber;
+    } else if (userInputRemoveAllWhitespace > maximumNumber) { // Check if input number is lesser than maximum number.
+        document.getElementById("output").innerText = "The input number " + userInputInterchange + " have to be less than: " + maximumNumber;
         console.log("input field have to be lesser");
-    }
-
-    else if (userInputInterchange === userInputRemoveAllWhitespace) { // Check and input value is valid.
-        document.getElementById("output").innerText = "The input is valid: " + userInputRemoveAllWhitespace;
-        console.log("input field is valid");
-    }
-
-    else {
+    } else if (parseInt(userInputRemoveAllWhitespace) === randomNumberGenerated) { // The guessed number is the same as the random generated number.
+        document.getElementById("output").innerText = "Congratulations! The guessed number " + userInputRemoveAllWhitespace + " is correct.";
+        console.log("The user guess is the same as the random generated number: " + randomNumberGenerated);
+        console.log("randomNumberGenerated: " + randomNumberGenerated);
+        console.log("userInputInterchange: " + userInputInterchange);
+        console.log("userInputRemoveAllWhitespace: " + userInputRemoveAllWhitespace);
+        roundCurrent = roundCurrent + 1;
+        document.getElementById("randomNumber").innerText = "Total number of guesses: " + roundCurrent;
+        console.log("roundCurrent: " + roundCurrent);
+    } else if (parseInt(userInputRemoveAllWhitespace) < randomNumberGenerated) { // The guessed number is to low.
+        document.getElementById("output").innerText = "The guessed number is to low";
+        console.log("randomNumberGenerated: " + randomNumberGenerated);
+        console.log("userInputRemoveAllWhitespace: " + userInputRemoveAllWhitespace);
+        roundCurrent = roundCurrent + 1;
+        document.getElementById("randomNumber").innerText = "Guess number: " + roundCurrent;
+        console.log("roundCurrent: " + roundCurrent);
+    } else if (parseInt(userInputRemoveAllWhitespace) > randomNumberGenerated) { // The guessed number is to high.
+        document.getElementById("output").innerText = "The guessed number is to high";
+        console.log("randomNumberGenerated: " + randomNumberGenerated);
+        console.log("userInputRemoveAllWhitespace: " + userInputRemoveAllWhitespace);
+        roundCurrent = roundCurrent + 1;
+        document.getElementById("randomNumber").innerText = "Guess number: " + roundCurrent;
+        console.log("roundCurrent: " + roundCurrent);
+    } else {
         document.getElementById("output").innerText = "else";
         console.log("else");
         console.log("userInput: " + userInput);
@@ -74,4 +101,14 @@ function randomNumberFunction() {
     // console.log("userInput.value: " + userInput.value);
 }
 
-document.getElementById("enterButton").addEventListener("click", randomNumberFunction);
+document.getElementById("enterButton").addEventListener("click", inputValidationFunction);
+
+// else if (userInputInterchange === userInputRemoveAllWhitespace) { // Check and input value is valid.
+//     document.getElementById("output").innerText = "The input is valid: " + userInputRemoveAllWhitespace;
+//     console.log("input field is valid");
+//     console.log("randomNumberGenerated: " + randomNumberGenerated);
+//     console.log("userInputRemoveAllWhitespace: " + userInputRemoveAllWhitespace);
+//     roundCurrent = roundCurrent + 1;
+//     document.getElementById("randomNumberVV").innerText = "Round number: " + roundCurrent;
+//     console.log("roundCurrentVV: " + roundCurrent);
+// }
