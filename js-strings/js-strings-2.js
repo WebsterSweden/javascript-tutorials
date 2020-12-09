dddb29659a4d6af6a88cfdcc19a4d027ced1677d
WebsterSweden <webster@webster.se>
2020-12-09 17:18:00 +0100

    Edit js-strings.

--- js-strings/js-strings-2.js
+++ js-strings/js-strings-2.js
@@ -8,3 +8,25 @@ window
 // bitwise, browser, convert, couch, devel, es6, eval, fudge, getset, long, node, single, this, white
 
 console.log(document.currentScript);
+
+function animateStringOneFunction() {
+    'use strict';
+
+    let animateStringOneInput = document.getElementById("animateStringOneInput").innerText;
+    let animateStringOneOutput = document.getElementById("animateStringOneOutput");
+    animateStringOneOutput.innerText = animateStringOneInput;
+    let animateArrayOneSplit = animateStringOneInput.split("");
+    document.getElementById("animateArrayOneSplit").innerText = animateArrayOneSplit;
+    let animateArrayOneReverse = animateArrayOneSplit.reverse();
+    document.getElementById("animateArrayOneReverse").innerText = animateArrayOneReverse;
+    let animateArrayOneLastElement = animateArrayOneReverse.pop();
+    document.getElementById("animateArrayOneLastElement").innerText = animateArrayOneLastElement;
+    let animateArrayOneFirstElement = animateArrayOneReverse.shift();
+    document.getElementById("animateArrayOneFirstElement").innerText = animateArrayOneFirstElement;
+    let animateArrayOneJoin = animateArrayOneReverse.join("");
+    document.getElementById("animateArrayOneJoin").innerText = animateArrayOneJoin;
+    let animateArrayOneConcat = animateArrayOneFirstElement.concat(animateArrayOneJoin, animateArrayOneLastElement);
+    document.getElementById("animateArrayOneConcat").innerText = animateArrayOneConcat;
+}
+
+document.getElementById("animateStringOneButton").addEventListener("click", animateStringOneFunction);
