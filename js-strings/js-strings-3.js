01d7a331cd74a1455e3b06d3d54882b531948638
WebsterSweden <webster@webster.se>
2020-12-09 17:26:09 +0100

    Edit js-strings.

---
+++ js-strings/js-strings-3.js
@@ -0,0 +1,10 @@
+/*jslint
+browser, devel, es6, for
+*/
+/*global
+window
+*/
+
+// bitwise, browser, convert, couch, devel, es6, eval, fudge, getset, long, node, single, this, white
+
+console.log(document.currentScript);
